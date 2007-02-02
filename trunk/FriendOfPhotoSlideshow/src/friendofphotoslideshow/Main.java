aba8d8c085fc5b787a071ec0a5fe1287b52af6d4
Wes.Billman <Wes.Billman@e8f001e0-7328-0410-a044-2d4fc57029ed>
2007-02-02 01:06:48 +0000

    Creating the project on code.google

---
+++ trunk/FriendOfPhotoSlideshow/src/friendofphotoslideshow/Main.java
@@ -0,0 +1,32 @@
+/*
+ * Main.java
+ *
+ * Created on February 1, 2007, 5:04 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package friendofphotoslideshow;
+
+/**
+ *
+ * @author my1939chevy
+ */
+public class Main
+{
+    
+    /** Creates a new instance of Main */
+    public Main()
+    {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+    
+}
